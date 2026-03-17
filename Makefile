975f24db4349ee75c16f3d90d25ee2f9e7a4aa62
ali-bennezia <bennezia.ali@gmail.com>
2026-03-17 20:31:04 +0100

    init

---
+++ Makefile
@@ -0,0 +1,27 @@
+# Paramètres
+TARGET = ./bin/mousejiggler-cli.exe
+CC = gcc
+
+# Options
+CFLAGS = -Wall -O2
+LDFLAGS =
+
+# Fichiers
+SRCS = ./src/main.c
+OBJS = ${SRCS:.c=.o}
+DEPS = 
+INCLUDE = 
+
+.PHONY: all clean
+
+all: $(TARGET)
+
+clean:
+	if exist .\bin\mousejiggler-cli.exe ( DEL .\bin\mousejiggler-cli.exe )
+	del /Q /S src\*.o
+
+%.o: %.c $(DEPS)
+	$(CC) $(CFLAGS) -c $< -o $@ 
+
+$(TARGET): $(OBJS)
+	$(CC) $^ -o $@ $(LDFLAGS)
---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "windows.h"
+
+static bool even = 0;
+void job()
+{
+    POINT pt = {0};
+    GetCursorPos( &pt );
+    SetCursorPos( pt.x+(even?1:-1), pt.y+(even?-1:1) );
+    SetCursorPos( pt.x, pt.y );
+    even = !even;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR pCmdLine, int nCmdShow)
+{
+    for (;;){
+        job();
+        Sleep(45000);
+    }
+    return 0;
+}
